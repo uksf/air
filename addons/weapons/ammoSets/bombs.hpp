3e3c4141b2caf36e9c521152bb7fd4dba875c3fd
Tim Beswick <contact.tim.here@gmail.com>
2026-01-20 21:24:44 +0000

    Buff bombs, add CRV PG

--- addons/weapons/ammoSets/bombs.hpp
+++ addons/weapons/ammoSets/bombs.hpp
@@ -1,6 +1,4 @@
-class ammo_Bomb_SDB : ammo_Bomb_SmallDiameterBase {
-    ace_rearm_dummy = QGVAR(ammo_Bomb_SDB);
-};
+
 class ammo_Bomb_LaserGuidedBase : LaserBombCore {
     hit = 10000; // 5000
     indirectHit = 2000; // 1100
@@ -13,4 +11,12 @@ class Bo_GBU12_LGB : ammo_Bomb_LaserGuidedBase {
 class CUP_Bo_GBU12_LGB : Bo_GBU12_LGB {
     model = "\A3\Weapons_F_EPC\Ammo\Bomb_04_fly_F";
     proxyShape = "\A3\Weapons_F_EPC\Ammo\Bomb_04_F";
+};
+class ammo_Bomb_SmallDiameterBase : ammo_Bomb_LaserGuidedBase {
+    hit = 4000; // 1800
+    indirectHit = 200; // 85
+    indirectHitRange = 6;  // 3
+};
+class ammo_Bomb_SDB : ammo_Bomb_SmallDiameterBase {
+    ace_rearm_dummy = QGVAR(ammo_Bomb_SDB);
 };
--- addons/weapons/magazineSets/rockets.hpp
+++ addons/weapons/magazineSets/rockets.hpp
@@ -12,7 +12,6 @@ class CUP_PylonPod_12Rnd_CRV7_FAT_M : CUP_19Rnd_CRV7_FAT_M {
     GVAR(pilotControl) = 1;
 };
 class CUP_PylonPod_19Rnd_CRV7_HE_M : CUP_PylonPod_19Rnd_CRV7_FAT_M {
-    hardpoints[] += { "B_CRV7" };
     model = QPATHTOEF(apache,PylonRocket_19Rnd_CRV7_HEISAP.p3d);
     CUP_PilotControl = 0;
     GVAR(pilotControl) = 1;
@@ -21,6 +20,19 @@ class CUP_PylonPod_12Rnd_CRV7_HE_M : CUP_PylonPod_12Rnd_CRV7_FAT_M {
     CUP_PilotControl = 0;
     GVAR(pilotControl) = 1;
 };
-class UK3CB_BAF_12Rnd_CRV7_Base_PG : VehicleMagazine {
+class UK3CB_BAF_PylonPod_19Rnd_CRV7_PG_HEISAP : UK3CB_BAF_PylonPod_19Rnd_CRV7_PG_Base {
+    hardpoints[] = { "B_CRV7" };
     initSpeed = 900;
+    CUP_PilotControl = 0;
+    GVAR(pilotControl) = 1;
+};
+class UK3CB_BAF_PylonPod_19Rnd_CRV7_PG_FAT : UK3CB_BAF_PylonPod_19Rnd_CRV7_PG_HEISAP {
+    CUP_PilotControl = 0;
+    GVAR(pilotControl) = 1;
+    pylonWeapon = "UK3CB_BAF_Missiles_CRV7_PG_FAT";
+};
+class UK3CB_BAF_PylonPod_19Rnd_CRV7_PG_GPF : UK3CB_BAF_PylonPod_19Rnd_CRV7_PG_HEISAP {
+    CUP_PilotControl = 0;
+    GVAR(pilotControl) = 1;
+    pylonWeapon = "UK3CB_BAF_Missiles_CRV7_PG_GPF";
 };
