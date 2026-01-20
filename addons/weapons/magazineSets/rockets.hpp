3223a2408b37f2240549640d70aca10c30762515
Tim Beswick <contact.tim.here@gmail.com>
2026-01-20 21:59:37 +0000

    Fix hardpoint

--- addons/weapons/magazineSets/rockets.hpp
+++ addons/weapons/magazineSets/rockets.hpp
@@ -12,6 +12,7 @@ class CUP_PylonPod_12Rnd_CRV7_FAT_M : CUP_19Rnd_CRV7_FAT_M {
     GVAR(pilotControl) = 1;
 };
 class CUP_PylonPod_19Rnd_CRV7_HE_M : CUP_PylonPod_19Rnd_CRV7_FAT_M {
+    hardpoints[] += { "B_CRV7" };
     model = QPATHTOEF(apache,PylonRocket_19Rnd_CRV7_HEISAP.p3d);
     CUP_PilotControl = 0;
     GVAR(pilotControl) = 1;
